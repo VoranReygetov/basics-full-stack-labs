e2a5191b830fcffd9e856ad07c5b17fe9ab110a5
VoranReygetov <voran009@gmail.com>
2024-05-07 10:07:46 +0300

    Added fourth lab

--- scripts3,4.js
+++ scripts3,4.js
@@ -307,3 +307,182 @@ function labConclusions_3(){
     ;
     document.getElementById("content").innerHTML = contentDiv;
 }
+
+let tableShown_3 = false;
+
+function change_lab_work_4(){
+    let contentDiv = 
+    `
+        <!-- Тема, мета -->
+        <div class="step-block" onclick="lab4_Meta()">
+            <h3>Тема, Мета, Місце розташування сайту, звіту</h3>
+        </div>
+        <!-- Зовнішній вигляд сайту -->
+        <div class="step-block" onclick="lab4_View()">
+            <h3>Зовнішній вигляд сайту
+        </div>
+        <div id="structure-3" class="structure">
+        </div>
+        <!-- Приклади CSS -->
+        <div class="step-block" onclick="lab4_Examples()">
+            <h3>Приклади CSS</h3>                
+        </div>
+        <div id="structure-1" class="structure">         
+        </div>
+        <!-- Висновки -->
+        <div class="step-block" onclick="lab4_Conclusions()">
+            <h3>Висновки</h3>
+        </div>
+    `;
+    document.getElementById("steps").innerHTML = contentDiv;
+    tableShown_3 = false;
+    lab4_Meta();
+}
+
+function lab4_Meta() {
+    // Отримуємо доступ до елемента div за допомогою його id
+    var contentDiv = document.getElementById("content");
+
+    // Змінюємо текст у елементі div
+    contentDiv.innerHTML = `
+        <ol>
+            <li>
+                <b>Тема</b>: АДАПТИВНА ВЕРСТКА. МЕДІА-ЗАПИТИ. МЕТАТЕГ VIEWPORT. СТРАТЕГІЯ MOBILE FIRST.
+            </li>
+            <li>
+            <b>Мета</b>: Адаптивна верстка. Медіа-запити. Медіа-типи. Медіа-функції. Метатег viewport. Інструменти розробника. Стратегія Mobile First. 
+            </li>
+            <li>
+            <b>Місце розташування</b>:
+                <ul>
+                    <li>
+                    <b>Сайту</b>: <a href='https://voranreygetov.github.io/ToyBox-WEB-4/'>Жива сторінка</a>
+                    </li>
+                    <li>
+                    <b>Звіт</b>: <a href='https://voranreygetov.github.io/basics-full-stack-labs/'>Жива сторінка</a>
+                    </li>
+                </ul>
+            </li>
+        </ol>
+    `;
+}
+
+function lab4_View() {
+    // Отримуємо доступ до елемента div за допомогою його id
+    let contentDiv = document.getElementById("structure-3");
+    if (tableShown_3) {
+        contentDiv.innerHTML = '';
+        tableShown_3 = false;
+    }
+    else{
+        // Змінюємо текст у елементі div
+        contentDiv.innerHTML = `
+            <div class="step-block-smaller" onclick="lab4_View_1()">Десктопна</div>
+            <div class="step-block-smaller" onclick="lab4_View_2()">Планшетна</div>
+            <div class="step-block-smaller" onclick="lab4_View_3()">Мобільна</div>
+        `;
+        tableShown_3 = true;
+    }
+}
+
+function lab4_View_1(){
+    let contentDiv = 
+    `
+    <div class="grid-with-header">
+        <h2>Десктопна версія на ширину екрана більшу за 1024px</h2>
+        <img src="./imgs/lab4/1/1-1.png" class="img-big-size" alt="">
+    </div>
+    `
+    ;
+    document.getElementById("content").innerHTML = contentDiv;
+}
+
+function lab4_View_2(){
+    let contentDiv = 
+    `
+    <div class="flexbox-with-header">
+        <h2>Планшетна версія на ширину екрана меншу за 1024px</h2>
+        <div>
+            <div class="flex-column-container"><img src="./imgs/lab4/1/1-2.png" class="img-big-size" alt=""></div>
+            <img src="./imgs/lab4/1/1-3.png" class="img-big-size" alt="">
+        </div>
+    </div>
+    `
+    ;
+    document.getElementById("content").innerHTML = contentDiv;
+}
+function lab4_View_3(){
+    let contentDiv = 
+    `
+    <div class="flexbox-with-header">
+        <h2>Мобільна версія на ширину екрана меншу за 768px</h2>
+        <div>
+        <div><img src="./imgs/lab4/1/1-4.png" class="img-big-size" alt=""></div>
+            <img src="./imgs/lab4/1/1-5.png" class="img-big-size" alt="">
+        </div>
+    </div>
+    `
+    ;
+    document.getElementById("content").innerHTML = contentDiv;
+}
+
+function lab4_Examples(){
+    let contentDiv = 
+    `
+    <div class="flexbox-with-header">
+        <h2>Приклади використання CSS для адаптивної верстки</h2>
+        <div>
+            <div class="flex-column-container">
+                <img src="./imgs/lab4/2/2-1.png" class="img-big-size" alt="">
+                Зміна паддінгу в макеті в залежності від розміру вікна
+            </div>
+            
+            <div class="flex-column-container">
+                <img src="./imgs/lab4/2/2-2.png" class="img-big-size" alt="">
+                Зміна лого 
+            </div>
+            
+            <div class="flex-column-container">
+                <img src="./imgs/lab4/2/2-3.png" class="img-big-size" alt="">
+                Зміна положення сайдбару
+            </div>
+            
+            <div class="flex-column-container">
+                <img src="./imgs/lab4/2/2-4.png" class="img-big-size" alt="">
+                Зміна слайдеру
+            </div>
+            
+            <div class="flex-column-container">
+                <img src="./imgs/lab4/2/2-10.png" class="img-big-size" alt="">
+                Зміна кількості продуктів в рядку з використанням AND
+            </div>
+            
+            <div class="flex-column-container">
+                <img src="./imgs/lab4/2/2-6.png" class="img-big-size" alt="">
+                Поява кнопок користувача знизу при мобільному варіанті
+            </div>                    
+        </div>
+    </div>
+    `
+    ;
+    document.getElementById("content").innerHTML = contentDiv;
+}
+
+function lab4_Conclusions(){
+    let contentDiv = 
+    `
+    У ході лабораторної роботи розробив адаптивну верстку з використанням медіа-запитів та CSS-правил. Основна мета роботи полягала в тому, щоб створити гнучкий дизайн, який коректно відображався б на різних пристроях: десктопах, планшетах і мобільних телефонах.
+    <p>Для визначення брейкпоінтів я використав стандартні значення:
+        <ul>
+            <li>768px для мобільних пристроїв
+            </li>
+            <li>1024px для планшетів
+            </li>
+            <li>Всі пристрої з більшою шириною вважалися десктопними
+            </li>
+        </ul>
+    </p>
+    `
+    ;
+    document.getElementById("content").innerHTML = contentDiv;
+}
